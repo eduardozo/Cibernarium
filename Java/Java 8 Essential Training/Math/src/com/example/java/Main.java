226468a8a13baca6478bb529d9ec163103b8e31a
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-24 10:12:14 +0200

    basic operations

--- Java/Java 8 Essential Training/Math/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/Math/src/com/example/java/Main.java
@@ -7,6 +7,21 @@ public class Main {
         int intValue1 = 56;
         int intValue2 = 42;
 
+        int result1 = intValue1 + intValue2;
+        System.out.println("Addition: " + result1);
+
+        int result2 = intValue1 - intValue2;
+        System.out.println("Subtraction: " + result2);
+
+        int result3 = intValue1 * intValue2;
+        System.out.println("Multiplication: " + result3);
+
+        double result4 = (double) intValue1 / intValue2;
+        System.out.println("Division: " + result4);
+
+        double result5 = (double) intValue1 % intValue2;
+        System.out.println("Remainder: " + result5);
+
     }
 
 }
