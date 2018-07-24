d30684c35d4cd308c4a460c04da87531c54b53ab
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-24 09:38:54 +0200

    precise calculation BigDecimal

--- Java/Java 8 Essential Training/Currency/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/Currency/src/com/example/java/Main.java
@@ -1,5 +1,7 @@
 package com.example.java;
 
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,7 +9,17 @@ public class Main {
         //imprecise calculation in java
         double value = .012;
         double psum = value + value + value;
-        System.out.println("sum: " + psum);
+        System.out.println("value: " + value);
+        System.out.println("sum of primitive: " + psum);
+
+        //precise calculation BigDecimal
+        String strValue = Double.toString(value);
+        System.out.println("\nstrValue: " + strValue);
+        BigDecimal bigValue = new BigDecimal(strValue);
+
+        BigDecimal bSum = bigValue.add(bigValue).add(bigValue);
+
+        System.out.println("sum of BigDecimals: " + bSum);
 
     }
 
