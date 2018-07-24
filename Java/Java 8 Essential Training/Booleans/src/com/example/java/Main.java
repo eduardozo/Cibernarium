d7b92d1cfc1f5eb72d3127310fd48c98e0053c52
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-24 10:40:02 +0200

    negation and Boolean parsed

--- Java/Java 8 Essential Training/Booleans/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/Booleans/src/com/example/java/Main.java
@@ -11,6 +11,18 @@ public class Main {
         System.out.println("The value of b1 is: " + b1);
         System.out.println("The value of b2 is: " + b2);
         System.out.println("The value of bDef is: " + bDef);
+
+        boolean b3 = !b1;
+        System.out.println("The value of b3 is: " + b3);
+
+        int il = 0;
+        boolean b4 = (il != 0);
+        System.out.println("The value of b4 is: " + b4);
+
+        String sBoolean = "true";
+        boolean parsed = Boolean.parseBoolean(sBoolean);
+        System.out.println("Parsed: " + parsed);
+
     }
 
 }
