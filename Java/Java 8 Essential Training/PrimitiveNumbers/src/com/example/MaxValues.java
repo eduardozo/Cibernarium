f1a818f133610c903add0f80bc599d040302b6ca
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-24 09:26:20 +0200

    init Currency

--- Java/Java 8 Essential Training/PrimitiveNumbers/src/com/example/MaxValues.java
+++ Java/Java 8 Essential Training/PrimitiveNumbers/src/com/example/MaxValues.java
@@ -1,4 +1,13 @@
 package com.example;
 
 public class MaxValues {
+    public static void main(String[] args) {
+        byte b = 127;
+
+        System.out.println("Byte: " + b);
+        if (b < Byte.MAX_VALUE) {
+            b++;
+        }
+        System.out.println("Byte: " + b);
+    }
 }
