c4bfbdb02dde3c6f3f3ff5db3e22f369443c8038
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-24 09:30:21 +0200

    imprecise calculation double

--- Java/Java 8 Essential Training/Currency/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/Currency/src/com/example/java/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //imprecise calculation in java
         double value = .012;
+        double psum = value + value + value;
+        System.out.println("sum: " + psum);
 
     }
 
