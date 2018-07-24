30c07207455892a44023c2658a7cf20174aece10
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-24 10:32:31 +0200

    static member and default value boolean

--- Java/Java 8 Essential Training/Booleans/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/Booleans/src/com/example/java/Main.java
@@ -2,8 +2,15 @@ package com.example.java;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static boolean bDef;
 
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+
+        System.out.println("The value of b1 is: " + b1);
+        System.out.println("The value of b2 is: " + b2);
+        System.out.println("The value of bDef is: " + bDef);
     }
 
 }
