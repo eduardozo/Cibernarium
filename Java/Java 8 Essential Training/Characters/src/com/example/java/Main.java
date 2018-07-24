1a6ab389aaedc3759776e717bffd2b3ed633cb53
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-24 10:55:23 +0200

    char operations with Character class

--- Java/Java 8 Essential Training/Characters/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/Characters/src/com/example/java/Main.java
@@ -4,6 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        char c1 = '1';
+        char c2 = '2';
+        char c3 = '3';
+
+        System.out.println("Char 1: " + c1);
+        System.out.println("Char 2: " + c2);
+        System.out.println("Char 3: " + c3);
+
+        char dolarSign = '\u0024';
+        System.out.println("Currency: " + dolarSign);
+
+        char a1 = 'a';
+        char a2 = 'b';
+        char a3 = 'c';
+
+        System.out.print(Character.toUpperCase(a1));
+        System.out.print(Character.toUpperCase(a2));
+        System.out.println(Character.toUpperCase(a3));
     }
 
 }
