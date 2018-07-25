c36939f8f24856ac65dd7f07075d4c83b55acdd1
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-25 14:10:32 +0200

    indexOf and substring

--- Java/Java 8 Essential Training/ParsingStrings/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/ParsingStrings/src/com/example/java/Main.java
@@ -5,7 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         String s1 = "Welcome to California!";
+        System.out.println("Length of string: " + s1.length());
 
+        int position = s1.indexOf("California");
+        System.out.println("Position of sudstring: " + position);
+
+        String sub = s1.substring(11);
+        System.out.println(sub);
+
+        String s2 = "Welcome!               ";
+        System.out.println(s2.length());
+        String s3 = s2.trim();
+        System.out.println(s3.length());
     }
 
 }
