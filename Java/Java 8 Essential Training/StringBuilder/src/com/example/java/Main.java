1ce1ce45866f20821f438726faf2e19f188f6253
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-07-25 10:59:42 +0200

    Scanner + StringBuilder

--- Java/Java 8 Essential Training/StringBuilder/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/StringBuilder/src/com/example/java/Main.java
@@ -18,9 +18,18 @@ public class Main {
         System.out.println(sb);
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a value: ");
+        System.out.print("Enter a value: ");
         String input = scanner.nextLine();
         System.out.println(input);
+
+        sb.delete(0, sb.length());
+        
+        System.out.println("Enter 3 Values: ");
+        for (int i = 0; i < 3; i++) {
+            input = scanner.nextLine();
+            sb.append(input + "\n");
+        }
+        System.out.println(sb);
     }
 
 }
