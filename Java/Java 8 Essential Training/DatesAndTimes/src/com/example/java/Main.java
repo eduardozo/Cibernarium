6aeeda01a1ddd60c7319f18f71579f1f90938bb4
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-03 12:36:27 +0200

    Java 8 API version

--- Java/Java 8 Essential Training/DatesAndTimes/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/DatesAndTimes/src/com/example/java/Main.java
@@ -1,7 +1,9 @@
 package com.example.java;
 
 import java.text.DateFormat;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -20,5 +22,17 @@ public class Main {
         DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.FULL);
         System.out.println(dateFormat.format(date1));
 
+        //Java 8 API version
+        LocalDateTime locale = LocalDateTime.now();
+        System.out.println(locale);
+
+        LocalDate localDate = LocalDate.of(2009, 1, 28);
+        System.out.println(localDate);
+
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE;
+        System.out.println(timeFormatter.format(localDate));
+
+        DateTimeFormatter timeFormatter1 = DateTimeFormatter.ofPattern("M/d/yyyy");
+        System.out.println(timeFormatter1.format(localDate));
     }
 }
