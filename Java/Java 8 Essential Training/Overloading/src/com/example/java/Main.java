30d05760e26ea897d63f951c6a8299d53e354bc9
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-04 11:07:56 +0200

    String ... values

--- Java/Java 8 Essential Training/Overloading/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/Overloading/src/com/example/java/Main.java
@@ -15,6 +15,9 @@ public class Main {
 
         double result2 = addValues(s1, s2, s3);
         System.out.println("2. Result for 3 values is: " + result2);
+
+        double result3 = addValues(s1, s2, s3, s1, s2, s3);
+        System.out.println("3. Result for multiple values is: " + result3);
     }
 
     private static double addValues(String s1, String s2) {
@@ -28,6 +31,15 @@ public class Main {
         double d2 = Double.parseDouble(s2);
         double d3 = Double.parseDouble(s3);
         return d1 + d2 + d3;
+    }
+
+    private static double addValues(String ... values) {
+        double result = 0;
+        for (String value : values) {
+            double d = Double.parseDouble(value);
+            result += d;
+        }
+        return result;
     }
 
     private static String getInput(String prompt) {
