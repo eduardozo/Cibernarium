c2fb2fc0dd2877f2c40471c6f9c0bd720997ec03
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-04 12:54:34 +0200

    ComplexCalc 4 operations

--- Java/Java 8 Essential Training/ComplexCalc/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/ComplexCalc/src/com/example/java/Main.java
@@ -12,17 +12,55 @@ public class Main {
 
         switch (opt) {
             case "+":
+                System.out.println("Result: " + addValues(s1, s2));
                 break;
             case "-":
+                System.out.println("Result: " + subValues(s1, s2));
                 break;
             case "*":
+                System.out.println("Result: " + multiValues(s1, s2));
                 break;
             case "/":
+                System.out.println("Result: " + divValues(s1, s2));
                 break;
 
                 default:
                     System.out.println("Unrecognized operation...");
         }
+    }
+
+    private static double divValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        double result = 0;
+        try {
+            if(d2 != 0) {
+                result = d1 / d2;
+            } else {
+                throw (new Exception("Second value most be distinct than 0."));
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return result;
+    }
+
+    private static double multiValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 * d2;
+    }
+
+    private static double subValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 - d2;
+    }
+
+    private static double addValues(String s1, String s2) {
+        double d1 = Double.parseDouble(s1);
+        double d2 = Double.parseDouble(s2);
+        return d1 + d2;
     }
 
     private static String getInput(String prompt) {
