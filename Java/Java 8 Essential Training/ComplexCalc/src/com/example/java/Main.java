da684c6b8f5f133e2e307f7ad3e0e838206bc0cf
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-05 09:30:46 +0200

    ComplexCalc done.

--- Java/Java 8 Essential Training/ComplexCalc/src/com/example/java/Main.java
+++ Java/Java 8 Essential Training/ComplexCalc/src/com/example/java/Main.java
@@ -10,31 +10,45 @@ public class Main {
         String s2 = getInput("2. Enter a numeric value: ");
         String opt = getInput("Choose an operation (+ - * /): ");
 
+        double d1 = parseNum(s1);
+        double d2 = parseNum(s2);
+
+        //if ()
         switch (opt) {
             case "+":
-                System.out.println("Result: " + addValues(s1, s2));
+                System.out.println("Result: " + addValues(d1, d2));
                 break;
             case "-":
-                System.out.println("Result: " + subValues(s1, s2));
+                System.out.println("Result: " + subValues(d1, d2));
                 break;
             case "*":
-                System.out.println("Result: " + multiValues(s1, s2));
+                System.out.println("Result: " + multiValues(d1, d2));
                 break;
             case "/":
-                System.out.println("Result: " + divValues(s1, s2));
+                System.out.println("Result: " + divValues(d1, d2));
                 break;
 
-                default:
-                    System.out.println("Unrecognized operation...");
+            default:
+                System.out.println("Unrecognized operation...");
         }
     }
 
-    private static double divValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
+    private static double parseNum(String s1) {
+        double d = 0;
+        try {
+            d = Double.parseDouble(s1);
+        } catch (NumberFormatException e) {
+            System.err.println("Number formating exception for input: " + s1);
+            //throw e;
+            System.exit(0);
+        }
+        return d;
+    }
+
+    private static double divValues(double d1, double d2) {
         double result = 0;
         try {
-            if(d2 != 0) {
+            if (d2 != 0) {
                 result = d1 / d2;
             } else {
                 throw (new Exception("Second value most be distinct than 0."));
@@ -45,21 +59,15 @@ public class Main {
         return result;
     }
 
-    private static double multiValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
+    private static double multiValues(double d1, double d2) {
         return d1 * d2;
     }
 
-    private static double subValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
+    private static double subValues(double d1, double d2) {
         return d1 - d2;
     }
 
-    private static double addValues(String s1, String s2) {
-        double d1 = Double.parseDouble(s1);
-        double d2 = Double.parseDouble(s2);
+    private static double addValues(double d1, double d2) {
         return d1 + d2;
     }
 
