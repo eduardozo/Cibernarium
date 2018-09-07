3650276a6952529524be7a66efb9c98a3d3d87e2
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-07 20:49:02 +0200

    Constants - static final type variables.

---
+++ Java/Java 8 Essential Training/Constants/src/com/example/java/OlivePress.java
@@ -0,0 +1,17 @@
+package com.example.java;
+
+import java.util.List;
+
+public class OlivePress {
+
+    public int getOil(List<Olive> olives) {
+
+        int totalOil = 0;
+        for (Olive o : olives) {
+            System.out.println(o.getName());
+            totalOil += o.crush();
+        }
+
+        return totalOil;
+    }
+}
