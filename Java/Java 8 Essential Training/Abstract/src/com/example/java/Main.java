9e5f3c65a94986bf549ebfa42e1a06693078a4ad
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-10 14:47:17 +0200

    Abstract init

---
+++ Java/Java 8 Essential Training/Abstract/src/com/example/java/Main.java
@@ -0,0 +1,39 @@
+package com.example.java;
+
+import com.example.java.model.Kalamata;
+import com.example.java.model.Ligurian;
+import com.example.java.model.Olive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Olive> olives = new ArrayList<>();
+        olives.add(new Kalamata());
+        olives.add(new Kalamata());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+        olives.add(new Ligurian());
+
+        Press press = new OlivePress();
+        press.setOil(30);
+        int totalOil = press.getOil(olives);
+        System.out.println("Total Olive Oil: " + totalOil);
+
+        /*Press p2 = new Press() {
+            @Override
+            public int getOil(List<Olive> olives) {
+                return 0;
+            }
+
+            @Override
+            public void setOil(int oil) {
+
+            }
+        };*/
+    }
+}
