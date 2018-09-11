2aae57c1df6e60445f0f38d1874fcd7f2afdfcde
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-11 20:59:04 +0200

    Power added to Propellants. Fase 2 done.

--- Java/Rocket/src/com/example/java/Main.java
+++ Java/Rocket/src/com/example/java/Main.java
@@ -12,24 +12,37 @@ public class Main {
 
         Rocket rocket1 = new Rocket("32WESSDS");
         List<Propellant> propellantsR1 = new ArrayList<>();
-        propellantsR1.add(new Propellant());
-        propellantsR1.add(new Propellant());
-        propellantsR1.add(new Propellant());
+        propellantsR1.add(new Propellant(10));
+        propellantsR1.add(new Propellant(30));
+        propellantsR1.add(new Propellant(80));
         rocket1.setPropellants(propellantsR1);
 
         System.out.println("Rocket 1 with code: " + rocket1.getCode() + " has " + rocket1.getPropellants().size() + " propellers.");
 
         Rocket rocket2 = new Rocket("LDSFJA32");
         List<Propellant> propellantsR2 = new ArrayList<>();
-        propellantsR2.add(new Propellant());
-        propellantsR2.add(new Propellant());
-        propellantsR2.add(new Propellant());
-        propellantsR2.add(new Propellant());
-        propellantsR2.add(new Propellant());
-        propellantsR2.add(new Propellant());
+        propellantsR2.add(new Propellant(30));
+        propellantsR2.add(new Propellant(40));
+        propellantsR2.add(new Propellant(50));
+        propellantsR2.add(new Propellant(50));
+        propellantsR2.add(new Propellant(30));
+        propellantsR2.add(new Propellant(10));
         rocket2.setPropellants(propellantsR2);
 
         System.out.println("Rocket 2 with code: " + rocket2.getCode() + " has " + rocket2.getPropellants().size() + " propellers.");
+        System.out.println();
+
+        System.out.print("Rocket: " + rocket1.getCode() + ", Potencia: ");
+        for (Propellant p : rocket1.getPropellants()) {
+            System.out.print(p.getPower() + ",");
+        }
+
+        System.out.println();
+
+        System.out.print("Rocket: " + rocket2.getCode() + ", Potencia: ");
+        for (Propellant p : rocket2.getPropellants()) {
+            System.out.print(p.getPower() + ",");
+        }
 
     }
 }
--- Java/Rocket/src/com/example/java/model/Propellant.java
+++ Java/Rocket/src/com/example/java/model/Propellant.java
@@ -2,6 +2,17 @@ package com.example.java.model;
 
 public class Propellant {
 
+    private int Power;
 
+    public Propellant(int power) {
+        Power = power;
+    }
 
+    public int getPower() {
+        return Power;
+    }
+
+    public void setPower(int power) {
+        Power = power;
+    }
 }
