c5324a78cb0e09853b3d164648375d089d6ad192
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-20 09:59:47 +0200

    CollectionExample List Integer + Arrays asList + Collections binarySearch

--- Java/Data Structures/CollectionExample/src/com/java/example/Main.java
+++ Java/Data Structures/CollectionExample/src/com/java/example/Main.java
@@ -1,8 +1,15 @@
 package com.java.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        List<Integer> list = Arrays.asList(1, 1, 2, 3, 5, 8, 13, 21, 34, 55);
+        System.out.println("Position of 21: " + Collections.binarySearch(list, 21));
+
     }
 }
