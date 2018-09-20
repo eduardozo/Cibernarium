20f3be32de5375a72c7006f5464695888799c166
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-20 12:21:19 +0200

    Stack pop + push

--- Java/Data Structures/Stack/src/com/java/example/Main.java
+++ Java/Data Structures/Stack/src/com/java/example/Main.java
@@ -1,10 +1,19 @@
 package com.java.example;
 
+import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 1; i <= 10; i++) {
+            stack.push(i);
+        }
 
-
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + ", ");
+        }
+        System.out.println("LIFT-OFF!!");
     }
 }
