33e100c53ed6988c40ed455a357e177f089263cd
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-20 11:06:11 +0200

    LinkedList add + addFirst

--- Java/Data Structures/LinkedList/src/com/java/example/Main.java
+++ Java/Data Structures/LinkedList/src/com/java/example/Main.java
@@ -1,10 +1,23 @@
 package com.java.example;
 
+import java.util.LinkedList;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        LinkedList<String> states = new LinkedList<>();
+        states.add("Alaska");
+        states.add("Arizona");
+        states.add("Arkansas");
+        states.add("Pennsylvania");
+        states.add("Colorado");
+        states.add("Connecticut");
+        states.add("Idaho");
+        states.add("Florida");
 
+        states.addFirst("Montana");
+        System.out.println(states);
 
     }
 }
