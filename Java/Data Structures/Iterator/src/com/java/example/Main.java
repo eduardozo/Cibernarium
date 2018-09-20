957361ddee34e8205bfd5e0933c87b372ce6a0f7
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-20 10:11:52 +0200

    Iterator hasNext + next

--- Java/Data Structures/Iterator/src/com/java/example/Main.java
+++ Java/Data Structures/Iterator/src/com/java/example/Main.java
@@ -1,10 +1,18 @@
 package com.java.example;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        Collection collection = Arrays.asList("red", "orange", "yellow", "green", "blue", "indigo", "violet");
+        Iterator iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
 
     }
 }
