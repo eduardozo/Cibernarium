6389ec6bf420b999cc31024d6a7a6a1ca0d2ca94
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-21 12:15:42 +0200

    Different quack classes implements QuackBehavior Interface.

---
+++ Java/Design Patterns/StrategyPattern/src/java/FakeQuack.java
@@ -0,0 +1,8 @@
+package java;
+
+public class FakeQuack implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Qwak");
+    }
+}
---
+++ Java/Design Patterns/StrategyPattern/src/java/MuteQuack.java
@@ -0,0 +1,8 @@
+package java;
+
+public class MuteQuack implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("-[Silence]-");
+    }
+}
