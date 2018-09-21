672ea639e7b20ae0bcac30a371c545f8fab242a6
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-21 16:28:43 +0200

    new Duck subclasses

---
+++ Java/Design Patterns/StrategyPattern/src/strategy/RubberDuck.java
@@ -0,0 +1,14 @@
+package strategy;
+
+public class RubberDuck extends Duck {
+
+    public RubberDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Squeak();
+    }
+
+    @Override
+    void display() {
+        System.out.println("I'm a rubber duckie");
+    }
+}
