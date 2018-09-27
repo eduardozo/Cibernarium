73e89de7fe6f7e36488078473cf24de860628e76
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-27 09:25:19 +0200

    weatherObservable packages, include its classes

---
+++ Java/Design Patterns/ObserverPattern/src/observer/weatherObservable/WeatherStation.java
@@ -0,0 +1,18 @@
+package observer.weatherObservable;
+
+public class WeatherStation {
+
+    public static void main(String[] args) {
+
+        WeatherData weatherData = new WeatherData();
+
+        CurrentConditionsDisplay currentCondition = new CurrentConditionsDisplay(weatherData);
+        ForecastDisplay forecast = new ForecastDisplay(weatherData);
+        StatisticsDisplay statistics = new StatisticsDisplay(weatherData);
+        HeatIndexDisplay heatIndex = new HeatIndexDisplay(weatherData);
+
+        weatherData.setMeasurements(80, 65, 30.4f);
+        weatherData.setMeasurements(82, 70, 29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
