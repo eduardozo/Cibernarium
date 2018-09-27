e62a2ecfdc9ed2fbd19cc53e2f9cdf456dd0877d
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-27 10:25:16 +0200

    WeatherStation updated, weatherObservable

--- Java/Design Patterns/ObserverPattern/src/observer/weatherObservable/WeatherStation.java
+++ Java/Design Patterns/ObserverPattern/src/observer/weatherObservable/WeatherStation.java
@@ -6,10 +6,10 @@ public class WeatherStation {
 
         WeatherData weatherData = new WeatherData();
 
-        CurrentConditionsDisplay currentCondition = new CurrentConditionsDisplay(weatherData);
-        ForecastDisplay forecast = new ForecastDisplay(weatherData);
-        StatisticsDisplay statistics = new StatisticsDisplay(weatherData);
-        HeatIndexDisplay heatIndex = new HeatIndexDisplay(weatherData);
+        new CurrentConditionsDisplay(weatherData);
+        new ForecastDisplay(weatherData);
+        new StatisticsDisplay(weatherData);
+        new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
