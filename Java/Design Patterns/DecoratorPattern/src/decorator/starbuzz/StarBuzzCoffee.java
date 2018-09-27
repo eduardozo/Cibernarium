3756cad835b18cf0558b1bf18aabc54119bd3aef
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-09-27 13:05:09 +0200

    Main renamed to StarBuzzCoffee

---
+++ Java/Design Patterns/DecoratorPattern/src/decorator/starbuzz/StarBuzzCoffee.java
@@ -0,0 +1,19 @@
+package decorator.starbuzz;
+
+public class StarBuzzCoffee {
+
+    public static void main(String[] args) {
+
+        Beverage beverage1 = new Espresso();
+
+        System.out.println(beverage1.getDescription() + " $" + beverage1.cost());
+
+        Beverage beverage2 = new HouseBlend();
+        beverage2 = new Milk(beverage2);
+        beverage2 = new Soy(beverage2);
+        beverage2 = new Mocha(beverage2);
+        System.out.println(beverage2.getDescription() + " $" + beverage2.cost());
+
+
+    }
+}
