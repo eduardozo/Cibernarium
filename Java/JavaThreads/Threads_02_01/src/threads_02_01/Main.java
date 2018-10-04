d1468b1c2cedffb782e2def8e580f4069db641e2
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-04 13:28:23 +0200

    Data race problem.

--- Java/JavaThreads/Threads_02_01/src/threads_02_01/Main.java
+++ Java/JavaThreads/Threads_02_01/src/threads_02_01/Main.java
@@ -1,8 +1,31 @@
 package threads_02_01;
 
 public class Main {
+    static double a = 10;
+    static double b;
 
     public static void main(String[] args) {
-	// write your code here
+
+        Runnable r1 = () -> {
+            if (a == 10) {
+                try {
+                    Thread.sleep(0);
+                    b = a / 2.0;
+                    System.out.println(Thread.currentThread().getName() + ": b = " + b);
+
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+        };
+
+        Runnable r2 = () -> { a = 12; };
+
+
+        Thread t1 = new Thread(r1, "Thread A");
+        Thread t2 = new Thread(r2, "Thread B");
+
+        t1.start();
+        t2.start();
     }
 }
