ba2dcedc713009376850b517a8c7b20087c8d77d
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-09 09:28:47 +0200

    Running Threads without Synchronized Block.

---
+++ Java/JavaThreads/Threads_02_04/src/threads_02_03/Main.java
@@ -0,0 +1,19 @@
+package threads_02_03;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Countdown CD = new Countdown();
+
+        Runnable r = () -> {
+            CD.printCount();
+        };
+
+        Thread one = new Thread(r, "One");
+        Thread two = new Thread(r, "Two");
+
+        one.start();
+        two.start();
+    }
+}
