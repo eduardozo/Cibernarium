81f0264290d88d04d1cef1973642fb4169cff851
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-09 10:03:56 +0200

    Wait and Notify API Threads.

---
+++ Java/JavaThreads/Threads_02_05/src/threads_02_05/Main.java
@@ -0,0 +1,34 @@
+package threads_02_05;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ThreadB b = new ThreadB();
+        b.start();
+        synchronized (b) {
+            try {
+                System.out.println("Waiting for second thread to complete...");
+                b.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Total is: " + b.total);
+        }
+    }
+
+}
+
+class ThreadB extends Thread {
+    public int total;
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            for (int i = 0; i < 10; i++) {
+                total += i;
+            }
+            notify();
+        }
+    }
+}
