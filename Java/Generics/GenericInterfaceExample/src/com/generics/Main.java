c2798e3715825e0844888bc08aa06bcf352fe6fa
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-10 14:28:34 +0200

    Testing create a new object list and add a String element.

---
+++ Java/Generics/GenericInterfaceExample/src/com/generics/Main.java
@@ -0,0 +1,10 @@
+package com.generics;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GenericList<String> list = new GenericList<>();
+        list.add("Good afternoon");
+    }
+}
