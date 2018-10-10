75cbcc2e6f21b1cd4f6d78d9a452f2298bf71b38
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-10 11:03:08 +0200

    creating two Pair objects and passing them to print(K k, V v) generic method.

--- Java/Generics/SyntaxExample/src/com/generics/Main.java
+++ Java/Generics/SyntaxExample/src/com/generics/Main.java
@@ -14,5 +14,17 @@ public class Main {
         a.add(12);
         //a.add("12"); //compile error
         Integer b = a.get(0);
+
+        Pair<Integer, String> p1 = new OrderedPair<>(1, "Apple");
+        Pair<String, Integer> p2 = new OrderedPair<>("Banana", 2);
+
+        print(p1.getKey(), p1.getValue());
+        print(p2.getKey(), p2.getValue());
+        print("Happy", "Birthday");
+
+    }
+    public static <K, V> void print(K key, V value){
+        System.out.println("Key: " + key);
+        System.out.println("Value: " + value);
     }
 }
