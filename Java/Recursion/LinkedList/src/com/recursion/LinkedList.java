10bca8c013d139e22bf24dec72d07c15874e6704
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-14 18:53:28 +0200

    Recursive method in LinkedList class completed.

---
+++ Java/Recursion/LinkedList/src/com/recursion/LinkedList.java
@@ -0,0 +1,26 @@
+package com.recursion;
+
+public class LinkedList {
+    static Node head;
+
+    public static void main(String[] args) {
+
+        Node node1 = new Node(1, null);
+        Node node2 = new Node(2, null);
+        Node node3 = new Node(3, null);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = null;
+        
+        printList(node1);
+
+    }
+
+    private static void printList(Node n) {
+        if (n != null) {
+            System.out.println(n.value + " ");
+            printList(n.next);
+        }
+    }
+}
