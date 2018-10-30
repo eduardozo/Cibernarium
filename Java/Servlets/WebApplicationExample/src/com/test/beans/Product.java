abd18cf958db076e008cfdb444059cb5c869c78d
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-30 13:31:05 +0100

    Product class bean

---
+++ Java/Servlets/WebApplicationExample/src/com/test/beans/Product.java
@@ -0,0 +1,38 @@
+package com.test.beans;
+
+/**
+ * Product bean
+ *
+ */
+public class Product {
+
+    private int productId;
+    private String productName;
+    private String productImgPath;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImgPath() {
+        return productImgPath;
+    }
+
+    public void setProductImgPath(String productImgPath) {
+        this.productImgPath = productImgPath;
+    }
+
+
+}
