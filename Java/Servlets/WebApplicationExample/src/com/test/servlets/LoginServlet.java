c53d9a046e9284aa2f3bb149565fef4d7036f1a0
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2018-10-31 13:46:45 +0100

    include instead of forward

--- Java/Servlets/WebApplicationExample/src/com/test/servlets/LoginServlet.java
+++ Java/Servlets/WebApplicationExample/src/com/test/servlets/LoginServlet.java
@@ -14,7 +14,10 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //dispatch the request to login.jsp resource
+        String html = "<html><h3>Please login...</h3></html>";
+        resp.getWriter().write(html);
+
         RequestDispatcher dispatcher = req.getRequestDispatcher("/html/login.jsp");
-        dispatcher.forward(req, resp);
+        dispatcher.include(req, resp);
     }
 }
